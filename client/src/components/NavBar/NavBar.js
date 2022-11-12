41efdd1b799c081bf0d83904d0f5d469cab48b32
Tom Tobar <cocdetombomb@gmail.com>
2022-11-11 18:54:36 -0600

    add React Router Links to nav

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,76 +1,70 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import {
-    MDBContainer,
-    MDBNavbar,
-    MDBNavbarBrand,
-    MDBNavbarToggler,
-    MDBIcon,
-    MDBNavbarNav,
-    MDBNavbarItem,
-    MDBNavbarLink,
-    MDBBtn,
-    MDBDropdown,
-    MDBDropdownToggle,
-    MDBDropdownMenu,
-    MDBDropdownItem,
-    MDBCollapse,
-  } from 'mdb-react-ui-kit';
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBCollapse,
+} from "mdb-react-ui-kit";
 
-function NavBar( {setUser} ) {
-    const [showBasic, setShowBasic] = useState(false)
+function NavBar({ setUser }) {
+  const [showBasic, setShowBasic] = useState(false);
 
-    const handleLogoutClick = () => {
-        const config = {
-            method: "DELETE"
-        }
+  const handleLogoutClick = () => {
+    const config = {
+      method: "DELETE",
+    };
 
-        fetch("/logout", config)
-            .then(resp => {
-                setUser({username: ""})
-                console.log(resp)
-            })
-    }
+    fetch("/logout", config).then((resp) => {
+      setUser({ username: "" });
+      console.log(resp);
+    });
+  };
 
-    return (
-        <MDBNavbar expand='lg' light bgColor='light'>
-          <MDBContainer fluid>
-            <MDBNavbarBrand href='#'>Brand</MDBNavbarBrand>
-    
-            <MDBNavbarToggler
-              aria-controls='navbarSupportedContent'
-              aria-expanded='false'
-              aria-label='Toggle navigation'
-              onClick={() => setShowBasic(!showBasic)}
-            >
-              <MDBIcon icon='bars' fas />
-            </MDBNavbarToggler>
-    
-            <MDBCollapse navbar show={showBasic}>
-              <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
+  return (
+    <MDBNavbar expand="lg" light bgColor="light">
+      <MDBContainer fluid>
+        <MDBNavbarBrand href="#">Brand</MDBNavbarBrand>
+
+        <MDBNavbarToggler
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={() => setShowBasic(!showBasic)}
+        >
+          <MDBIcon icon="bars" fas />
+        </MDBNavbarToggler>
+
+        <MDBCollapse navbar show={showBasic}>
+          <MDBNavbarNav className="mr-auto mb-2 mb-lg-0">
+            <MDBNavbarItem>
+              <Link to="/">Home</Link>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <Link to="/login">Login</Link>
+            </MDBNavbarItem>
+            {/* </MDBNavbarItem>
                 <MDBNavbarItem>
-                  <MDBNavbarLink active aria-current='page' href='/'>
-                    Home
-                  </MDBNavbarLink>
-                  <MDBNavbarLink active aria-current='page' href='/login'>
-                    Login
-                  </MDBNavbarLink>
-                {/* </MDBNavbarItem>
-                <MDBNavbarItem>
-                  <MDBNavbarLink href='#'>Link</MDBNavbarLink>
+                <MDBNavbarLink href='#'>Link</MDBNavbarLink>
                 </MDBNavbarItem>
-    
-                <MDBNavbarItem> */}
-                  <MDBNavbarLink href='#' tabIndex={-1} aria-disabled='true' onClick={handleLogoutClick}>
-                    Logout
-                  </MDBNavbarLink>
-                  <MDBNavbarLink active aria-current='page' href='/signup'>
-                    Sign up
-                  </MDBNavbarLink>
-                </MDBNavbarItem>
-              </MDBNavbarNav>
-            </MDBCollapse>
-          </MDBContainer>
-        </MDBNavbar>
-      );
+                
+            <MDBNavbarItem> */}
+
+            <MDBNavbarItem>
+              <Link onClick={handleLogoutClick}>Logout</Link>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <Link to="/signup">Sign up</Link>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
 }
-export default NavBar
+export default NavBar;
